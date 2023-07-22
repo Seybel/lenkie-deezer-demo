fe18b2a5457d3a9d2151a18edb504affb33f5bcd
Seybel <seyibello31@gmail.com>
2023-07-22 23:04:06 +0100

    Feat/setup api config

---
+++ src/services/apiConfig.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const PROXY_URL = "https://cors-anywhere.herokuapp.com";
+const BASE_URL = "https://api.deezer.com";
+
+const http = axios.create({
+  baseURL: `${PROXY_URL}/${BASE_URL}`,
+  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+export default http;
